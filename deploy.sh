a554fb52f1f22cd22fa866509b0068576ef8cc56
waterloocode <330316259@qq.com>
2018-10-23 08:32:09 +0800

    demo

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,12 @@
-cp  env.example .env
-docker-compose build php-cli && \
-docker-compose build --no-cache workspace
+ln -s $HOMR/.ssh workspace/ssh
+&& docker-compose build \
+&& docker-compose up -d workspace php-fpm redis nginx \
+&& docker-compose exec workspace rm -rf shop \
+&& docker-compose exec workspace git clone git@gitee.com:allet_liability/laravel-shop.git shop \
+&& cd code/shop  \
+&& cp .env.docker-compose.example .env \
+&& touch database/database.sqlite \
+&& cd ../../ \
+&& docker-compose exec --user=www-data  php-fpm composer -vvv -d=/www/shop install \
+&& docker-compose exec --user=www-data  php-fpm php /www/shop/artisan ccb:deploy -vvv \
+&& docker-compose exec --user=www-data  php-fpm php /www/shop/artisan migrate:refresh -vvv
